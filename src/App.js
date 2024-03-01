249ec383300d24d1aaf19f1fe3e7089effe68d3d
Michael J. Kazin <mkazin@gmail.com>
2024-03-01 09:58:07 -0500

    Completed ImageCarousel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import Accordion from './components/accordion';
-import RandomColor from './components/randomColor';
-import StarRating from './components/starRating';
+// import Accordion from './components/accordion';
+import ImageCarousel from './components/imageCarousel';
+// import RandomColor from './components/randomColor';
+// import StarRating from './components/starRating';
 
 function App() {
   return (
     <div className="App">
-          <StarRating starCount={10}></StarRating>
-          <RandomColor></RandomColor>
-          <Accordion></Accordion>
+          <ImageCarousel url='https://picsum.photos/v2/list?page=12&limit=10'></ImageCarousel>
+          {/* <StarRating starCount={10}></StarRating> */}
+          {/* <RandomColor></RandomColor> */}
+          {/* <Accordion></Accordion> */}
     </div>
   );
 }
---
+++ src/components/imageCarousel/index.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react"
+import { BsArrowLeftCircleFill , BsArrowRightCircleFill} from "react-icons/bs"
+import "./styles.css"
+
+export default function ImageCarousel({url, limit}) {
+
+    const [images, setImages] = useState([])
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const [errorMsg, setErrorMsg] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    async function fetchImages(url) {
+        try {
+            console.log(`Fetching image data from ${url}`)
+            setLoading(true)
+
+            const response = await fetch(url)
+            const data = await response.json()
+
+            if (data) {
+                setImages(data)
+                setLoading(false)
+            } 
+        } catch(e) {
+            setErrorMsg(e.message)
+            setLoading(false)
+        }
+    }
+
+    useEffect( () => {
+         if (url !== '') fetchImages(url)
+    }, [url])
+
+    if (loading) {
+        return <div>Loading image data...</div>
+    }
+
+    if (errorMsg) {
+        return <div>Error loading image data: {errorMsg}</div>
+    }
+    
+    return <div className="container">
+        <BsArrowLeftCircleFill 
+            className={"arrow arrow-left" + (currentSlide === 0 ? " disabled-arrow" : "")}
+            onClick={ () => setCurrentSlide(currentSlide - (currentSlide > 0 ? 1 : 0)) } 
+            />
+        {
+
+            images && images.length ?
+                images.map( (imageData, index) => {
+                    return <img 
+                        key={imageData.id}
+                        index={index}
+                        className={ index !== currentSlide ? "hidden-image" : "current-image" }
+                        src={imageData.download_url}
+                        alt={imageData.author}
+                        >
+                    </img>
+                    
+            })
+            : null
+        }
+        <BsArrowRightCircleFill 
+            className={"arrow arrow-right" + (currentSlide >= images.length - 1 ? " disabled-arrow" : "")}
+            onClick={ () => setCurrentSlide(currentSlide + (currentSlide >= images.length - 1 ? 0 : 1)) } 
+            />
+
+        <span className="circle-indicators">
+            {
+                images && images.length ?
+                images.map( (_, index) =>
+                <button 
+                    key={index}
+                    className={"circle-indicator " + (index===currentSlide ? "current-indicator" : "")}
+                    onClick={ () => setCurrentSlide(index) }
+                />
+                )
+
+                : null
+            }
+        </span>
+    </div>
+}
