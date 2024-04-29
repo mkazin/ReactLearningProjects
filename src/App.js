1632077bb673299a3e02f9c81e608c9281236a50
Michael J. Kazin <mkazin@gmail.com>
2024-04-28 21:06:49 -0400

    Dependency updates, unit test fix

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import LoadMore from './components/loadMore';
 function App() {
   return (
     <div className="App">
+
+      { /* DON'T COMMENT OUT THIS HEADER it's used in the App.test.js */}
+      <h1>I learn react</h1>
+      { /* DON'T COMMENT OUT THIS HEADER - it's used in the App.test.js */}
+
+
       <LoadMore url="https://dummyjson.com/products?limit=LIMIT&skip=SKIP" limit={30}/>
       {/* <ImageCarousel url='https://picsum.photos/v2/list?page=12&limit=10'></ImageCarousel> */}
       {/* <StarRating starCount={10}></StarRating> */}
