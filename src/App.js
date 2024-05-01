d2bd4e3be9056fe085a6c60bd542e3b3e0f6cd65
Michael J. Kazin <mkazin@gmail.com>
2024-05-01 09:17:58 -0400

    Demo other components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 // import Accordion from './components/accordion';
-// import ImageCarousel from './components/imageCarousel';
-import LoadMore from './components/loadMore';
+import ImageCarousel from './components/imageCarousel';
+// import LoadMore from './components/loadMore';
 // import RandomColor from './components/randomColor';
-// import StarRating from './components/starRating';
+import StarRating from './components/starRating';
+
+// import TreeView from './components/treeView';
+// import menus from './components/treeView/data.js';
 
 function App() {
+  // console.log(menus)
+
   return (
     <div className="App">
 
@@ -14,9 +19,10 @@ function App() {
       { /* DON'T COMMENT OUT THIS HEADER - it's used in the App.test.js */}
 
 
-      <LoadMore url="https://dummyjson.com/products?limit=LIMIT&skip=SKIP" limit={30}/>
-      {/* <ImageCarousel url='https://picsum.photos/v2/list?page=12&limit=10'></ImageCarousel> */}
-      {/* <StarRating starCount={10}></StarRating> */}
+      {/* <TreeView menus={menus} /> */}
+      {/* <LoadMore url="https://dummyjson.com/products?limit=LIMIT&skip=SKIP" limit={30}/> */}
+      <ImageCarousel url='https://picsum.photos/v2/list?page=12&limit=10'></ImageCarousel>
+      <StarRating starCount={10}></StarRating>
       {/* <RandomColor></RandomColor> */}
       {/* <Accordion></Accordion> */}
     </div>
