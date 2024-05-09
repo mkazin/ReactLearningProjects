31b5e1c0dd8971596ff65cb400355badf1adcb86
Michael J. Kazin <mkazin@gmail.com>
2024-05-09 08:03:59 -0400

    Trying to get LogRocket/React to work

--- src/App.js
+++ src/App.js
@@ -21,15 +21,24 @@ import setupLogRocketReact from 'logrocket-react';
 function App() {
   // console.log(menus)
 
-  LogRocket.init(process.env.REACT_APP_LOGROCKET_ID);
-  setupLogRocketReact(LogRocket);
+  const enableLogRocket =
+    process.env.NODE_ENV === 'production' && process.env.REACT_APP_LOG_ROCKET_APP_ID;
+  const logRocketAppId = process.env.REACT_APP_LOGROCKET_ID
+  if (enableLogRocket && logRocketAppId) {
+    LogRocket.init(logRocketAppId);
+    setupLogRocketReact(LogRocket);
+  }
 
   return (
     <div className="App">
-
-      { /* DON'T COMMENT OUT THIS HEADER it's used in the App.test.js */}
+      { /* DON'T COMMENT OUT THIS HEADER - it's used in the App.test.js */}
       <h1>I learn react</h1>
       { /* DON'T COMMENT OUT THIS HEADER - it's used in the App.test.js */}
+      <table>
+        <th><td>Variable</td><td>Value</td></th>
+        <tr><td>NODE_ENV:</td><td>{process.env.NODE_ENV}</td></tr>
+        <tr><td>REACT_APP_LOGROCKET_ID</td><td>{process.env.REACT_APP_LOGROCKET_ID}</td></tr>
+      </table>
 
 
       {/* <TreeView menus={menus} /> */}
